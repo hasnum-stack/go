4aa1e9c5b03f69cd949dc27d88ba0365618162ad
hasnum <hasnum@163.com>
2022-05-20 00:28:10 +0800

    feat

--- Gin/demo02/main.go
+++ Gin/demo02/main.go
@@ -1,11 +1,34 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type test struct {
+	name string
+	age  int
+}
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.String(200, "zzh123456")
 	})
+
+	a := "123"
+	Pa := &a
+	*Pa = "456"
+	fmt.Printf("a: %v\n", Pa)
+
+	ATest := &test{
+		name: "zzh",
+		age:  19,
+	}
+	BTest := ATest
+	BTest.age = 20
+	fmt.Printf("ATest: %v\n", ATest)
+
 	r.Run(":8081")
 }
